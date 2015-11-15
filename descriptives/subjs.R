3ae67825c341025da1cd2ae5579cf47bbe4973f1
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-11-14 20:07:28 -0600

    Added parallel z plot

--- descriptives/subjs.R
+++ descriptives/subjs.R
@@ -58,3 +58,25 @@ ggplot(subjs_parallel, aes(x = rank_type, y = rank_value, color = subj_id)) +
   scale_y_continuous("Rank", breaks = rank_axis_breaks) +
   subj_theme
 ggsave("descriptives/subjs-parallel.png")
+
+z_score <- function(x) (x - mean(x, na.rm = TRUE))/sd(x, na.rm = TRUE)
+
+subjs_parallel_z <- subjs %>%
+  mutate(
+    rt_z = z_score(rt), 
+    error_z = z_score(error)
+  ) %>% 
+  select(subj_id, rt_z, error_z) %>%
+  gather(measure, z_score, -subj_id) %>%
+  mutate(measure = factor(measure, levels = c("error_z", "rt_z")))
+
+ggplot(subjs_parallel_z, aes(x = measure, y = z_score, color = subj_id)) +
+  geom_line(aes(group = subj_id)) +
+  geom_text(aes(label = subj_id),
+            data = filter(subjs_parallel_z, measure == "error_z"),
+            hjust = 1) +
+  scale_x_discrete("", labels = c("Error", "RT")) +
+  scale_y_continuous("z-score") +
+  coord_cartesian(ylim = c(-2.7, 2.7)) +
+  subj_theme
+ggsave("descriptives/subjs-parallel-z.png")
